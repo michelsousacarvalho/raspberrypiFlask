040e1eb15d3bcb3cdf0511740dda8a7e0e2a7691
Wesley Thesolin de Faria <wesleythesolin@gmail.com>
2015-11-23 22:31:26 -0200

    Alter

--- app.py
+++ app.py
@@ -91,19 +91,21 @@ def quartolamp(action):
     # time.sleep(0.2)
 
     if action == "off":
+        ser1.open()
+        ser1.write("<y10p3>")
+        ser1.close()
         time.sleep(0.5)
         ser1.open()
         ser1.write("<y10p3>")
         ser1.close()
-        # time.sleep(0.5)
-        # ser1.open()
-        # ser1.write("<y10p3>")
-        # ser1.close()
         templateData['lampquarto1'] = False
         templateData['statusLampQuarto1'] = "Apagado"
 
 
     if action == "on":
+        ser1.open()
+        ser1.write("<y1255p3>")
+        ser1.close()
         time.sleep(0.5)
         ser1.open()
         ser1.write("<y1255p3>")
